63b6f5a7d03144f2dc9f10d3953c7a865c4d852a
DevHieu <hieudd2090@gmail.com>
2024-10-24 00:25:47 +0700

    reomve '/' in client url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 const corsOptions = {
-  origin: "https://guradian.netlify.app/",
+  origin: "https://guradian.netlify.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
